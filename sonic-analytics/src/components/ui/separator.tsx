40b6cc4137704843805ccc077f9df8057f68e861
jumpinaiapp <jumpinaiapp@gmail.com>
2026-03-05 23:32:14 +0800

    fix: 補齊 UI 套件並校正網頁結構

--- sonic-analytics/src/components/ui/separator.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import * as React from "react";
-
-export function Separator({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`w-full border-t ${className}`} {...props} />;
-}
